76fd2c18d26ccffd2253a5b9ef78df8997675e0d
oleg <8888kb8888@gmail.com>
2022-11-15 11:03:47 +0300

    Авторизация в index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Telegraf, Markup } = require("telegraf")
 require("dotenv").config()
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf('5591066660:AAFER-NgLuinlmw0JWl4SV2Wwk9OdMPBndQ')
 bot.start((ctx) => ctx.reply('Вас приветствует Робот помошник!!!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('Like'))
@@ -9,3 +9,5 @@ bot.hears('Как дела?', (ctx) => ctx.reply('Отлично'))
 bot.launch()
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+// process.env.BOT_TOKEN
